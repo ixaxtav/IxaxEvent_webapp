32ff8b23ce689e8533a3fec4c1363db0fd77690e
Ixax Tavarez <i.tavarez@umiami.edu>
2018-12-22 08:19:31 +0000

    adding counter

--- src/js/component/functional/Carrousel.jsx
+++ src/js/component/functional/Carrousel.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { SliderCarrousel } from "./slider.jsx";
+import Counter from "./Counter.jsx";
 
 export function Carrousel() {
 	return (
 		<div style={{ position: "relative" }}>
 			<SliderCarrousel />
 
-			<div className="artist_name" style={{ boxSizing: "content-box" }}>
-				<div className="wrapper">Ariana Grande</div>
+			<div className="artist_name">
+				<div className="wrapper" style={{ boxSizing: "content-box" }}>
+					Ariana Grande
+				</div>
 			</div>
+
+			<Counter />
 		</div>
 	);
 }
