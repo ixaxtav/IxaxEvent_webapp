095bddf742220708f470793461c94da139316c9e
Ixax Tavarez <i.tavarez@umiami.edu>
2018-12-28 11:44:40 +0000

    few fixes

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,7 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
+import Fade from "react-reveal/Fade";
 
-export class Footer extends Component {
-	render() {
-		return <h2 className="text-center">Footer</h2>;
-	}
-}
+const Footer = () => {
+	return (
+		<footer className="bck_red">
+			<Fade delay={500}>
+				<div className="font_righteous footer_logo_venue">
+					The Venue
+				</div>
+				<div className="footer_copyright">
+					The venue 2018.All rights reserved.
+				</div>
+			</Fade>
+		</footer>
+	);
+};
+
+export default Footer;
